7dd31cda654e22b02b400b35622653a0ed4ea096
Diego <diegogub@gmail.com>
2017-02-21 13:17:32 +0400

    japan network working

--- rakuten/export.php
+++ rakuten/export.php
@@ -2,6 +2,15 @@
 
 include 'rakuten/auth.php';
 
+# campaign match
+$campaigns = array(
+  # JAPAN NETWORK
+  "2880" => "9d5ea621",
+  "41434" => "017eff65",
+  "36689" => "e1a2de45",
+  "36690" => "f05d491a"
+);
+
 ## get new token obj
 $obj = RakutenAuth("Basic MnJBaVBDNlZpN0ZiNXdaUzRuajcwTHlHakVJYTpubVp6TWtudnFIRjlaX3FfYXFfV2dITTBBdThh",array(
   "grant_type" => "password",
@@ -10,11 +19,27 @@ $obj = RakutenAuth("Basic MnJBaVBDNlZpN0ZiNXdaUzRuajcwTHlHakVJYTpubVp6TWtudnFIRj
   "scope" => "3330749",
 ));
 
+// TODO improve time managment
+$start_date = "2017-02-21%2002:00:00:00";
+
 # export commissions
-$c = new Curl("https://api.rakutenmarketing.com/events/1.0/transactions",array("Authorization: Bearer ".$obj->access_token));
+$c = new Curl("https://api.rakutenmarketing.com/events/1.0/transactions?process_date_start=".$start_date,array("Authorization: Bearer ".$obj->access_token));
 $c->request("GET");
 for($i=0;$i < count($c->obj);$i++){
-  // print each transaction
-    // TODO do s2s request
-    var_dump($c->obj[$i]);
+    $actualCamp = $campaigns[$c->obj[$i]->advertiser_id];
+    $orderid = $c->obj[$i]->order_id;
+    $totalCost = $c->obj[$i]->sale_amount;
+    $commission = $c->obj[$i]->commissions;
+    $refid = $c->obj[$i]->u1;
+    $currency = $c->obj[$i]->currency;
+
+    // parse date
+    $darr = date_parse($c->obj[$i]->transaction_date);
+    $date  = $darr["year"] . "-" . $darr["month"] . "-" . $darr["day"];
+
+	  $url = "http://track.clickwise.net/pb?TotalCost=$totalCost&OrderId=$orderid&Commission=$commission&CampaignID=$actualCamp&RefId=$refid&Date=$date&Currency=$currency";
+    echo $url ."\n";
+    $s2s = new Curl($url);
+    $s2s->request("GET");
+    var_dump($s2s->obj);
 }
